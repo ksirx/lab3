b6ea1cdd68b4ebab97493e52454189d430276e9e
ksirx <xnavarro@ucsd.edu>
2023-04-19 17:57:08 -0700

    ArrayExamples Update
    For reversed, fixed issue where returns the incorrect array and made sure to update the correct array. Also, for reverseInPlace, made sure to not undo changes (through arr.length / 2) and save a temp of arr[i] to update the last half of the array. Also, updated the tests to ensure that they work as intended.

--- ArrayTests.java
+++ ArrayTests.java
@@ -3,16 +3,36 @@ import org.junit.*;
 
 public class ArrayTests {
 	@Test 
-	public void testReverseInPlace() {
+	public void testReverseInPlace1() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = { 1,2,3,4,5 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 5,4,3,2,1 }, input1);
+	}
+
 
   @Test
-  public void testReversed() {
+  public void testReversed1() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = { 5,4,3,2,1 };
+    assertArrayEquals(new int[]{ 1,2,3,4,5 }, ArrayExamples.reversed(input1));
+  }
+
+
+  @Test
+  public void testAverageWithoutLowest1() {
+    double[] inputDub = { 80.0, 90.00, 100.00, 70.00 };
+    assertArrayEquals(double 5.0, ArrayExamples.averageWithoutLowest(inputDub));
   }
 }
